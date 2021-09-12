b98a3451c233fe7f585a97f2d5e0218026f07014
dangngocthanh <helomilo123@gmail.com>
2021-09-12 18:24:01 +0700

    c

--- brick.js
+++ brick.js
@@ -1,6 +1,5 @@
 class Brick {
     constructor() {
-        this.status = true
         this.width = 50
         this.height = 10
         this.ctx=document.getElementById('MyCanvas').getContext('2d')
@@ -10,9 +9,7 @@ class Brick {
         this.ctx.beginPath()
         this.ctx.fillStyle = 'white'
         this.ctx.rect(x, y, this.width, this.height)
-        // this.x+=60
         this.ctx.fill()
         this.ctx.closePath()
-        // this.x+=50
     }
 }
